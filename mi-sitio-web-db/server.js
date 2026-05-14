5c5d3bf27b8e3f371dacbdab1b372672090bf77b
GERMANGUILLEN <quimicoindustrial1995@gmail.com>
2026-05-13 23:03:05 -0600

    Fix PostgreSQL SSL

--- mi-sitio-web-db/server.js
+++ mi-sitio-web-db/server.js
@@ -8,9 +8,9 @@ console.log(process.env.DATABASE_URL);
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+ssl: process.env.RAILWAY_ENVIRONMENT
+  ? { rejectUnauthorized: false }
+  : false
 });
 
 app.use(express.static('public'));
