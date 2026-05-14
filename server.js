b1f3a314c31031bbf1edb87998234d79eed6db3f
GERMANGUILLEN <quimicoindustrial1995@gmail.com>
2026-05-13 22:49:20 -0600

    Debug PostgreSQL

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+app.use(express.static('public'));
+
+app.get('/datos', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.json(result.rows);
+} catch (err) {
+
+  console.error("ERROR POSTGRESQL:");
+  console.error(err);
+
+  res.status(500).send(err.message);
+}
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
