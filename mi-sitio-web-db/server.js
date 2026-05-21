92a63a543d507496544a01c0cab616f68f01222d
GERMANGUILLEN <quimicoindustrial1995@gmail.com>
2026-05-21 11:12:57 -0600

    Remove SSL config

--- mi-sitio-web-db/server.js
+++ mi-sitio-web-db/server.js
@@ -36,12 +36,6 @@ app.get('/datos', async (req, res) => {
 
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-
-  console.log(
-    `Servidor corriendo en puerto ${PORT}`
-  );
-
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
 });
