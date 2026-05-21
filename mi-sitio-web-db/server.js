34ec156ce1a61608b3aa50df31551d4197db5933
GERMANGUILLEN <quimicoindustrial1995@gmail.com>
2026-05-21 11:17:28 -0600

    Simplificar conexión PostgreSQL

--- mi-sitio-web-db/server.js
+++ mi-sitio-web-db/server.js
@@ -6,36 +6,40 @@ const app = express();
 console.log("DATABASE_URL:");
 console.log(process.env.DATABASE_URL);
 
+// Conexión a PostgreSQL
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  },
-
-  connectionTimeoutMillis: 5000
+    connectionString: process.env.DATABASE_URL
 });
 
 app.use(express.static('public'));
 
+// Ruta para consultar la base de datos
 app.get('/datos', async (req, res) => {
 
-  try {
+    try {
 
-    const result =
-      await pool.query('SELECT NOW()');
+        const result =
+            await pool.query('SELECT NOW()');
 
-    res.json(result.rows);
+        res.json(result.rows);
 
-  } catch (err) {
+    } catch (err) {
 
-    console.error("ERROR POSTGRESQL:");
-    console.error(err);
+        console.error("ERROR POSTGRESQL:");
+        console.error(err);
 
-    res.status(500).send(err.message);
-  }
+        res.status(500).send(err.message);
+
+    }
 
 });
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+
+    console.log(
+        `Servidor corriendo en puerto ${PORT}`
+    );
+
 });
